170b978006ec0ca34dcc9956929a7f9d37f77509
nickwells <wellsnick11@yahoo.com>
2023-03-19 13:57:21 -0700

    patient back db changed

--- app/serializers/patient_serializer.rb
+++ app/serializers/patient_serializer.rb
@@ -1,3 +1,6 @@
 class PatientSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :name, :phone, :dob
+
+  has_many :appointments
+  has_many :users, through: :appointments
 end
--- db/migrate/20230317195118_create_patients.rb
+++ db/migrate/20230317195118_create_patients.rb
@@ -2,7 +2,8 @@ class CreatePatients < ActiveRecord::Migration[7.0]
   def change
     create_table :patients do |t|
       t.string :name
-      t.string :birthdate
+      t.string :phone
+      t.string :dob
       t.integer :user_id
 
       t.timestamps
