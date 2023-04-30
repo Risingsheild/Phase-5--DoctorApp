176e2449fdea9a99c1e8fd3c5bdee89d9e7599b5
nickwells <wellsnick11@yahoo.com>
2023-04-30 12:51:21 -0700

    fixing render issue

--- app/models/prescription.rb
+++ app/models/prescription.rb
@@ -1,6 +1,6 @@
 class Prescription < ApplicationRecord
-    belongs_to :patient
-    belongs_to :user
+    belongs_to :patient, inverse_of: :prescriptions
+    belongs_to :user, inverse_of: :prescriptions
 
     validates :name, :refills, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
 
     has_many :appointments
     has_many :patients, -> {distinct}, through: :appointments
-    has_many :prescriptions, through: :patients, dependent: :destroy
+    has_many :prescriptions, through: :patients
 
     validates :username, uniqueness: true, presence: true
 end
