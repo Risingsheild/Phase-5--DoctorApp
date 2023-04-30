36a471b7117cbc828f70cedfb6da9760942b1b6b
nickwells <wellsnick11@yahoo.com>
2023-04-30 13:27:20 -0700

    fixing render issue

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,7 +1,7 @@
 class Patient < ApplicationRecord
     has_many :appointments
     has_many :users, through: :appointments
-    has_many :prescriptions dependent: :destroy
+    has_many :prescriptions, dependent: :destroy
 
     validates :first_name, :last_name, :phone, :dob, presence: true
 
