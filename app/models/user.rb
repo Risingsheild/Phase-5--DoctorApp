419e2c72ca0b3e56a8ab66b719d4bd170761ce92
nickwells <wellsnick11@yahoo.com>
2023-04-30 11:50:22 -0700

    fixing render issue

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
     has_secure_password
 
-    has_many :appointments
-    has_many :patients, -> {distinct}, through: :appointments
+    has_many :appointments, dependent: :destroy
+    has_many :patients, -> {distinct}, through: :appointments, dependent: :destroy
     has_many :prescriptions, through: :patients
 
     validates :username, uniqueness: true, presence: true
