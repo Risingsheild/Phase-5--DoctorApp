b29423a0fd690a8e05e0813070923369debd09f8
nickwells <wellsnick11@yahoo.com>
2023-04-30 13:24:23 -0700

    fixing render issue

--- app/models/prescription.rb
+++ app/models/prescription.rb
@@ -1,6 +1,5 @@
 class Prescription < ApplicationRecord
-    belongs_to :patient, inverse_of: :prescriptions
-    belongs_to :user, inverse_of: :prescriptions
-
+    belongs_to :patient
+    belongs_to :user
     validates :name, :refills, presence: true
 end
