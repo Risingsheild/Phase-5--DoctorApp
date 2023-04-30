a6735af5eebe57edcbd7077a89790244ecdc6199
nickwells <wellsnick11@yahoo.com>
2023-04-30 15:42:57 -0700

    Fixing Render Issue

--- app/models/prescription.rb
+++ app/models/prescription.rb
@@ -1,5 +1,7 @@
 class Prescription < ApplicationRecord
     belongs_to :patient
     belongs_to :user
+
+    
     validates :name, :refills, presence: true
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,5 +2,5 @@ class UserSerializer < ActiveModel::Serializer
   attributes :id, :username, :password_digest
   has_many :appointments
   has_many :patients
-  has_many :prescriptions, through: :patients
+  # has_many :prescriptions, through: :patients
 end
